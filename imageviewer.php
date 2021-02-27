6d436c1baf4148b8557e21e781e464fb706e5879
Nate Sutton <natemsutton@gmail.com>
2021-02-27 18:10:30 -0500

    added image zoom buttons

--- imageviewer.php
+++ imageviewer.php
@@ -29,14 +29,84 @@
 		width: auto;  
 		height: 100%; 
 	} 	
+	.responsive-image2 {  
+		width: 100%;  
+		height: auto; 
+	} 
+	.responsive-image3 {  
+		width: 1600px;  
+		height: auto; 
+	}
+	.responsive-image4 {  
+		width: 1800px;  
+		height: auto; 
+	}
+	.responsive-image5 {  
+		width: 2000px;  
+		height: auto; 
+	}
+	.responsive-image6 {  
+		width: auto;  
+		height: auto; 
+	}			
 	.closebutton {
 		position:absolute;position:fixed;top:0px;right:0px;font-size:36px;
 	}
+	.zoomin {
+		position:absolute;position:fixed;top:70px;right:0px;font-size:36px;
+	}
+	.zoomout {
+		position:absolute;position:fixed;top:140px;right:0px;font-size:36px;
+	}	
 </style>
 <script>
 	function closewindow() {
 		window.close();
 	}
+	function zoomin() {
+		if ( document.getElementById("image").classList.contains('responsive-image') ) {
+			document.getElementById("image").classList.remove('responsive-image');
+			document.getElementById("image").classList.add('responsive-image2');
+		}
+		else if ( document.getElementById("image").classList.contains('responsive-image2') ) {
+			document.getElementById("image").classList.remove('responsive-image2');
+			document.getElementById("image").classList.add('responsive-image3');
+		}
+		else if ( document.getElementById("image").classList.contains('responsive-image3') ) {
+			document.getElementById("image").classList.remove('responsive-image3');
+			document.getElementById("image").classList.add('responsive-image4');
+		}
+		else if ( document.getElementById("image").classList.contains('responsive-image4') ) {
+			document.getElementById("image").classList.remove('responsive-image4');
+			document.getElementById("image").classList.add('responsive-image5');
+		}		
+		else if ( document.getElementById("image").classList.contains('responsive-image5') ) {
+			document.getElementById("image").classList.remove('responsive-image5');
+			document.getElementById("image").classList.add('responsive-image6');
+		}			
+	}
+	function zoomout() {
+		if ( document.getElementById("image").classList.contains('responsive-image6') ) {
+			document.getElementById("image").classList.remove('responsive-image6');
+			document.getElementById("image").classList.add('responsive-image5');
+		}
+		else if ( document.getElementById("image").classList.contains('responsive-image5') ) {
+			document.getElementById("image").classList.remove('responsive-image5');
+			document.getElementById("image").classList.add('responsive-image4');
+		}
+		else if ( document.getElementById("image").classList.contains('responsive-image4') ) {
+			document.getElementById("image").classList.remove('responsive-image4');
+			document.getElementById("image").classList.add('responsive-image3');
+		}
+		else if ( document.getElementById("image").classList.contains('responsive-image3') ) {
+			document.getElementById("image").classList.remove('responsive-image3');
+			document.getElementById("image").classList.add('responsive-image2');
+		}		
+		else if ( document.getElementById("image").classList.contains('responsive-image2') ) {
+			document.getElementById("image").classList.remove('responsive-image2');
+			document.getElementById("image").classList.add('responsive-image');
+		}			
+	}	
 </script>
 </head>
 <body>
@@ -44,7 +114,7 @@
 <?php
 	if (isset($_REQUEST['image'])) {
 		$image = str_replace('file://', '', $_REQUEST['image']);
-		echo "<img src='$image' class='responsive-image'>";
+		echo "<img src='$image' id='image' class='responsive-image'>";
 	}
 ?>
 </center>
@@ -53,5 +123,7 @@
 <input type="hidden" name="basedir" id="basedir" value=<?php echo "'".$_REQUEST['basedirimg']."'" ?>>
 </form>
 <input type="button" value=" x " class="closebutton" onclick="javascript:closewindow()" />
+<input type="button" value=" + " class="zoomin" onclick="javascript:zoomin()" />
+<input type="button" value=" - " class="zoomout" onclick="javascript:zoomout()" />
 </body>
 </html>
